37723fc923ccb9f93af34d8cd104e604d3151660
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-11-24 01:55:51 +0200

    Adjusts card parameters

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,31 +1,28 @@
 import PropTypes from 'prop-types';
+import user from '../user.json';
 
 export const Profile = () => {
   return (
     <div class="profile">
       <div class="description">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-          class="avatar"
-        />
-        <p class="name">Petra Marica</p>
-        <p class="tag">@pmarica</p>
-        <p class="location">Salvador, Brasil</p>
+        <img src={user.avatar} alt="User avatar" class="avatar" />
+        <p class="name">{user.username}</p>
+        <p class="tag">{user.tag}</p>
+        <p class="location">{user.location}</p>
       </div>
 
       <ul class="stats">
         <li>
           <span class="label">Followers</span>
-          <span class="quantity">1000</span>
+          <span class="quantity">{user.stats.followers}</span>
         </li>
         <li>
           <span class="label">Views</span>
-          <span class="quantity">2000</span>
+          <span class="quantity">{user.stats.views}</span>
         </li>
         <li>
           <span class="label">Likes</span>
-          <span class="quantity">3000</span>
+          <span class="quantity">{user.stats.likes}</span>
         </li>
       </ul>
     </div>
