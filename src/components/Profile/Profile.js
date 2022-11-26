d14f294ac1bf9f66549ae517bff298c397a94eed
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-11-26 14:27:17 +0200

    Adding props to Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -37,14 +37,14 @@ export const Profile = () => {
   );
 };
 
-// Profile.propTypes = {
-//   username: PropTypes.string.isRequired,
-//   tag: PropTypes.string.isRequired,
-//   location: PropTypes.string.isRequired,
-//   avatar: PropTypes.string.isRequired,
-//   stats: PropTypes.shape({
-//     followers: PropTypes.number.isRequired,
-//     views: PropTypes.number.isRequired,
-//     likes: PropTypes.number.isRequired,
-//   }),
-// };
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
