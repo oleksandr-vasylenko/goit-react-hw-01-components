9a0944286c141a59a3f5ea3c31904a7dd3b4f910
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-11-26 12:17:12 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
 import { Profile } from './Profile';
 import { GlobalStyle } from './GlobalStyle';
+import user from '../data/user';
 
 export const App = () => {
   return (
     <>
       <div className="profile-thumb">
-        <Profile />
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
       </div>
       <GlobalStyle />
     </>
