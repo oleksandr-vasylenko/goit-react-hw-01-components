90e00ffd328b60d5ea4ebfb6600fedaf093212f1
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-11-29 15:00:47 +0200

    Adjusts Global Styles

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -3,7 +3,9 @@ import 'modern-normalize';
 
 export const GlobalStyle = createGlobalStyle`
  body {
-  padding: 16px;
+  display: flex;
+  justify-content: center;
+  padding: 24px;
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
